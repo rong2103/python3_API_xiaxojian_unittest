f06e8f3d3b01a16b4ba8cb935c8c5a85dee230ac
keilantra <978792720@qq.com>
2018-04-02 17:58:11 +0800

    1

--- common/dirconfig.py
+++
@@ -1,9 +0,0 @@
-import os
-
-cur_dir = os.path.split(os.path.abspath(__file__))[0]
-
-testcase_dir = cur_dir.replace("common","testdata")
-
-html_report = cur_dir.replace("common","testreport")
-
-logger_dir = cur_dir.replace("common","logs")
